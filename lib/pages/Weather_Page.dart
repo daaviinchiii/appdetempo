517b8464476c4828d706a6e26c3a2c85c708c6f7
daaviinchiii <dvdcardoso23@gmail.com>
2024-04-17 19:07:36 +0000

    Initial commit

---
+++ lib/pages/Weather_Page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:appdetempo/services/Weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+
+    final _WeatherService
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
